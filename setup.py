aca64119a91307e8ed1d9920ecafd03e5b9ef2c5
Grey Li <withlihui@gmail.com>
2017-07-03 17:07:15 +0800

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='Flask-Avatars',
+    version='1.0',
+    description='All avatar generators in one place.',
+    url='https://github.com/greyli/flask-avatars',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    license='MIT',
+    include_package_data=True
+)
